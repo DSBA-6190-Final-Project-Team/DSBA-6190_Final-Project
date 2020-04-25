81b825d2b37aedcdcbd8ebcbff4ff6a5a5a80d53
EC2 Default User <ec2-user@ip-172-31-85-229.ec2.internal>
2020-04-25 00:51:39 +0000

    Updates Loadtest

--- load_test/locustfile.py
+++ load_test/locustfile.py
@@ -1,11 +1,21 @@
   
-from locust import Locust, HttpLocust, TaskSet, task, between
+from locust import HttpLocust, TaskSet, task, between
+import os
 
-class MyTaskSet(TaskSet):
+header = {
+    "Content-Type" : "image/jpg"
+}
+
+payload_path = "payload_files/img_1.jpg"
+
+payload = open(payload_path, "rb").read()
+
+class UserBehavior(TaskSet):
     @task(1)
-    
-    def index(self):
-        self.client.get("/")
+    def post_tests(self):
+        self.client.post(path = "/Test/predict", 
+                         data = payload, 
+                         headers = header)
         
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
