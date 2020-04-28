0299231bc51053d63717d9307b2721a97fefb99f
EC2 Default User <ec2-user@ip-172-31-85-229.ec2.internal>
2020-04-28 02:31:57 +0000

    Load Test Preliminary

--- load_test/locustfile.py
+++ load_test/locustfile.py
@@ -1,22 +1,44 @@
-  
 from locust import HttpLocust, TaskSet, task, between
 import os
+import logging
 
-header = {
-    "Content-Type" : "image/jpg"
-}
+LOG = logging.getLogger()
+LOG.setLevel(logging.INFO)
+
+host_url = "https://96gfw8ry96.execute-api.us-east-1.amazonaws.com"
+
+post_path='Test/predict'
+
+#headers = {
+#    "Content-Type": "image/jpg"
+#}
 
 payload_path = "payload_files/img_1.jpg"
 
 payload = open(payload_path, "rb").read()
 
-class UserBehavior(TaskSet):
-    @task(1)
-    def post_tests(self):
-        self.client.post(path = "/Test/predict", 
-                         data = payload, 
-                         headers = header)
+class BaseTaskSet(TaskSet):
+    
+    def on_start(self):
+        # Called everytime when a simulated user starts executing TaskSet class
+        self.headers = {
+            'content-type': "image/jpg"
+        }
         
+        LOG.info('Headers: {}'.format(self.headers))
+
+
+class UserBehavior(BaseTaskSet):
+    @task(1)
+    def post_test(self):
+        response = self.client.post(url=os.path.join(host_url, post_path),
+                         data=payload,
+                         headers = self.headers
+        )
+
+        LOG.info("Return Payload Status: {}".format(response))
+
 class WebsiteUser(HttpLocust):
-    task_set = UserBehavior
-    wait_time = between(5.0, 15)
+    host=host_url
+    task_set=UserBehavior
+    wait_time=between(5.0, 15)
